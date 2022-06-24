d8ce89dea750490b453de0e9d44865482fae2571
Matheus Felipe Rocha Dias <m.dias@univille.br>
2022-06-24 17:32:46 -0300

    inicio

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import LoginComponent from './components/LoginComponent.vue';
+import DashboardComponent from './components/DashboardComponent.vue'
+
+</script>
+
+<template>
+
+<LoginComponent />
+<DashboardComponent />
+<!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+</template>
+
+<style>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        teal: colors.teal,
+        cyan: colors.cyan,
+      },
+    },
+  },
+  plugins: [
+    // ...
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
